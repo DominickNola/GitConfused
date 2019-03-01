b036127e1b440f6d4bf26368660e3806f314c305
Dominick Licciardi <dominick.nola@gmail.com>
2019-03-01 01:04:23 -0700

    edit methods.py

--- jvpm/methods.py
+++ jvpm/methods.py
@@ -157,27 +157,27 @@ class Methods():
 
     def iload_0():
         """iload: push variable[0] to the Stack"""
-        pushing = VARIABLES[0]
+        pushing = OPERANDS[0]
         S.push(pushing)
-        print('iload_0: Load VARIABLES[0] on the Stack for processing.')
+        print('iload_0: Load OPERANDS[0] on the Stack for processing.')
 
     def iload_1():
         """iload: push variable[1] to the Stack"""
-        pushing = VARIABLES[1]
+        pushing = OPERANDS[1]
         S.push(pushing)
-        print('iload_1: Load VARIABLES[1] on the Stack for processing.')
+        print('iload_1: Load OPERANDS[1] on the Stack for processing.')
 
     def iload_2():
         """iload: push variable[2] to the Stack"""
-        pushing2 = VARIABLES[2]
+        pushing2 = OPERANDS[2]
         S.push(pushing2)
-        print('iload_2: Load VARIABLES[2] on the Stack for processing.')
+        print('iload_2: Load OPERANDS[2] on the Stack for processing.')
 
     def iload_3():
         """iload: push variable[3] to the Stack"""
-        pushing3 = VARIABLES[3]
+        pushing3 = OPERANDS[3]
         S.push(pushing3)
-        print('iload_3: Load VARIABLES[3] on the Stack for processing.')
+        print('iload_3: Load OPERANDS[3] on the Stack for processing.')
 
     # def impdep1():
     #     print('impdep1')
@@ -263,33 +263,33 @@ class Methods():
     #     print('method57')
 
     def istore_0():
-        """istore_0: store int value into VARIABLES[0]"""
+        """istore_0: store int value into OPERANDS[0]"""
         popped = S.pop()
-        VARIABLES.pop(0) # remove the assigned 0 from the [0]position
-        VARIABLES.insert(0, popped)
+        OPERANDS.pop(0) # remove the assigned 0 from the [0]position
+        OPERANDS.insert(0, popped)
         print("istore_0: Popped " + str(popped) +
-              " from Stack and stored in VARIABLES[0] in jvpm_methods.py.")
+              " from Stack and stored in OPERANDS[0] in jvpm_methods.py.")
 
     def istore_1():
-        """istore_1: store int value into VARIABLES[1]"""
+        """istore_1: store int value into OPERANDS[1]"""
         popped = S.pop()
-        VARIABLES.insert(1, popped)
+        OPERANDS.insert(1, popped)
         print("istore_1: Popped " + str(popped) +
-              " from Stack and stored in VARIABLES[1] in jvpm_methods.py.")
+              " from Stack and stored in OPERANDS[1] in jvpm_methods.py.")
 
     def istore_2():
-        """istore_2: store int value into VARIABLES[2]"""
+        """istore_2: store int value into OPERANDS[2]"""
         popped = S.pop()
-        VARIABLES.insert(2, popped)
+        OPERANDS.insert(2, popped)
         print("istore_2: Popped " + str(popped) +
-              " from Stack and stored in VARIABLES[2] in jvpm_methods.py.")
+              " from Stack and stored in OPERANDS[2] in jvpm_methods.py.")
 
     def istore_3():
-        """istore_3: store int value into VARIABLES[3]"""
+        """istore_3: store int value into OPERANDS[3]"""
         popped = S.pop()
-        VARIABLES.insert(3, popped)
+        OPERANDS.insert(3, popped)
         print("istore_3: Popped " + str(popped) +
-              " from Stack and stored in VARIABLES[3] in jvpm_methods.py,"
+              " from Stack and stored in OPERANDS[3] in jvpm_methods.py,"
               "\n>>>>>>>>>>>>>>>>>>>>>>>> c = " + str(popped) + " <<<<<<<<<<<<<<<<<<<<<<<<<<<")
         print(">>>> Top of Stack is now " + str(S.size()) + ".")
 
