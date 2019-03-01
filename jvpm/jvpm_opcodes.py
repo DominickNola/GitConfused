3e4c97f56a7493eb79ac48efefeabbbf85de2302
Dominick Licciardi <dominick.nola@gmail.com>
2019-03-01 03:08:02 -0700

    Update jvpm_opcodes.py

--- jvpm/jvpm_opcodes.py
+++ jvpm/jvpm_opcodes.py
@@ -88,7 +88,7 @@ class OpCodes():
             # send "opcall" to the new Method() class instead of the value of opcall - iconst_1. 
             # method_call.opcall()
         
-            result = getattr(methods, opcall)()
+            result = getattr(Methods(), opcall)()
         
             index += 1
         print()
