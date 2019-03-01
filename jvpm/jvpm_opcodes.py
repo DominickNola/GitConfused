4e96ca44728ae7436885b6c00b7508dea5ac1cf5
Dominick Licciardi <dominick.nola@gmail.com>
2019-03-01 02:06:51 -0700

    Update jvpm_opcodes.py

--- jvpm/jvpm_opcodes.py
+++ jvpm/jvpm_opcodes.py
@@ -80,7 +80,7 @@ class OpCodes():
             jvpm_methods.opcode_methods(opcall)
             # Im able to call the method in Methods() class in methods.py using the opcade name
             method_call.iconst_3()
-            # but when I try to opcall it fails, even though opcall = iconst_1.
+            # but when I try to call opcall it fails, even though opcall = iconst_1.
             # method_call.opcall()
             index += 1
         print()
