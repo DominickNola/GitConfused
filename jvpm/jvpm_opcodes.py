17996c12bfccfb1922988a0d36ebda9bfe53a98b
Dominick Licciardi <dominick.nola@gmail.com>
2019-03-04 15:16:53 -0700

    Update jvpm_opcodes.py

--- jvpm/jvpm_opcodes.py
+++ jvpm/jvpm_opcodes.py
@@ -44,7 +44,7 @@ class OpCodes():
     def __init__(self):
 #         self.opcodes = ['04', '3c', '05', '3d', '1b', '1c', '60', '1c', '68', '1c', '6c', '1c',
 #                         '64', '3e']
-        self.opcodes = ['06', '3c', '04', '3d']
+        self.opcodes = ['06', '3c', '04', '3d', '1b']
 
 
         """
