4e577231e0dc93c1a6c1bd9fa3b35b0573e6e847
Dominick Licciardi <dominick.nola@gmail.com>
2019-03-04 18:15:34 -0700

    Update jvpm_opcodes_2.py

--- jvpm/jvpm_opcodes_2.py
+++ jvpm/jvpm_opcodes_2.py
@@ -87,7 +87,7 @@ if '__main__' == __name__:
           '\ntest2_logic.java'
           '\nint a = 2;'
           '\nint b = 2;'
-          '\nint c = ((((((a & b) >> b) << a) | a) % b) ^ a);\n')
+          '\nint c = ((((((a & b) << b) >> a) | a) % b) ^ a);\n')
 
     O = OpCodes()
     O.dict_search(OpCodeMethods()) # will need to pass through the oject that holds all the methods
